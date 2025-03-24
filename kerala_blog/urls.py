0811f7ad071d875ded1c38d161c5b644125a3e1d
jisbinJose <jisbin2003@gmail.com>
2025-03-24 19:17:43 +0530

    new version added

--- kerala_blog/urls.py
+++ kerala_blog/urls.py
@@ -27,4 +27,9 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/blog/', permanent=True)),  # Redirect root to /blog/
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/blog/'), name='logout'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
